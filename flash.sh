7672912fb64e4d8ece289a19ccdecc294b336429
Kevin Thomas <kevin@mytechnotalent.com>
2022-09-17 18:53:17 -0400

    Update

---
+++ flash.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cp main.py pico/micropython/ports/rp2/modules
+cd pico/micropython/ports/rp2 && make clean
+make
+cp build-PICO/firmware.uf2 ../../../../
+cp build-PICO/firmware.uf2 /Volumes/RPI-RP2 > /dev/null 2>&1
+cd ../../../../
---
+++ main.py
@@ -0,0 +1,9 @@
+from utime import sleep
+
+name = foo
+number = 42
+
+while True:
+    print(name)
+    print(number)
+    sleep(5)

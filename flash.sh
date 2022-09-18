a1359759560adcb45fd29b97f998e017d2c8278f
Kevin Thomas <kevin@mytechnotalent.com>
2022-09-18 09:04:06 -0400

    Update

--- flash.sh
+++ flash.sh
@@ -3,6 +3,7 @@
 cp main.py pico/micropython/ports/rp2/modules
 cd pico/micropython/ports/rp2 && make clean
 make
+cp build-PICO/firmware.elf ../../../../
 cp build-PICO/firmware.uf2 ../../../../
 cp build-PICO/firmware.uf2 /Volumes/RPI-RP2 > /dev/null 2>&1
 cd ../../../../

a1cd928d2ff11169d5bacc427a921e55f370ec66
Kevin Thomas <kevin@mytechnotalent.com>
2022-09-19 07:58:46 -0400

    Update

---
+++ pico/micropython/ports/rp2/build-PICO/CMakeFiles/firmware.dir/Users/kevinthomas/Documents/pico-micropython-debug-template/pico/micropython/lib/pico-sdk/src/rp2_common/pico_float/float_math.c.obj.d
@@ -0,0 +1,32 @@
+CMakeFiles/firmware.dir/Users/kevinthomas/Documents/pico-micropython-debug-template/pico/micropython/lib/pico-sdk/src/rp2_common/pico_float/float_math.c.obj: \
+ /Users/kevinthomas/Documents/pico-micropython-debug-template/pico/micropython/lib/pico-sdk/src/rp2_common/pico_float/float_math.c \
+ /Users/kevinthomas/Documents/pico-micropython-debug-template/pico/micropython/lib/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /Users/kevinthomas/Documents/pico-micropython-debug-template/pico/micropython/lib/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ /opt/local/lib/gcc/arm-none-eabi/12.1.0/include/stdbool.h \
+ /opt/local/arm-none-eabi/include/assert.h \
+ /opt/local/arm-none-eabi/include/_ansi.h \
+ /opt/local/arm-none-eabi/include/newlib.h \
+ /opt/local/arm-none-eabi/include/_newlib_version.h \
+ /opt/local/arm-none-eabi/include/sys/config.h \
+ /opt/local/arm-none-eabi/include/machine/ieeefp.h \
+ /opt/local/arm-none-eabi/include/sys/features.h \
+ /opt/local/lib/gcc/arm-none-eabi/12.1.0/include/stdint.h \
+ /opt/local/arm-none-eabi/include/stdint.h \
+ /opt/local/arm-none-eabi/include/machine/_default_types.h \
+ /opt/local/arm-none-eabi/include/sys/_intsup.h \
+ /opt/local/arm-none-eabi/include/sys/_stdint.h \
+ /opt/local/lib/gcc/arm-none-eabi/12.1.0/include/stddef.h \
+ /Users/kevinthomas/Documents/pico-micropython-debug-template/pico/micropython/lib/pico-sdk/src/rp2_common/pico_float/include/pico/float.h \
+ /opt/local/arm-none-eabi/include/math.h \
+ /opt/local/arm-none-eabi/include/sys/reent.h \
+ /opt/local/arm-none-eabi/include/_ansi.h \
+ /opt/local/arm-none-eabi/include/sys/_types.h \
+ /opt/local/arm-none-eabi/include/machine/_types.h \
+ /opt/local/arm-none-eabi/include/sys/lock.h \
+ /opt/local/arm-none-eabi/include/sys/cdefs.h \
+ /opt/local/lib/gcc/arm-none-eabi/12.1.0/include/float.h \
+ /Users/kevinthomas/Documents/pico-micropython-debug-template/pico/micropython/lib/pico-sdk/src/rp2_common/pico_bootrom/include/pico/bootrom/sf_table.h \
+ /Users/kevinthomas/Documents/pico-micropython-debug-template/pico/micropython/lib/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ /Users/kevinthomas/Documents/pico-micropython-debug-template/pico/micropython/lib/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /Users/kevinthomas/Documents/pico-micropython-debug-template/pico/micropython/lib/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /Users/kevinthomas/Documents/pico-micropython-debug-template/pico/micropython/lib/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h
---
+++ pico/micropython/ports/rp2/build-PICO/CMakeFiles/firmware.dir/pendsv.c.obj.d
@@ -0,0 +1,23 @@
+CMakeFiles/firmware.dir/pendsv.c.obj: \
+ /Users/kevinthomas/Documents/pico-micropython-debug-template/pico/micropython/ports/rp2/pendsv.c \
+ /opt/local/arm-none-eabi/include/assert.h \
+ /opt/local/arm-none-eabi/include/_ansi.h \
+ /opt/local/arm-none-eabi/include/newlib.h \
+ /opt/local/arm-none-eabi/include/_newlib_version.h \
+ /opt/local/arm-none-eabi/include/sys/config.h \
+ /opt/local/arm-none-eabi/include/machine/ieeefp.h \
+ /opt/local/arm-none-eabi/include/sys/features.h \
+ /Users/kevinthomas/Documents/pico-micropython-debug-template/pico/micropython/ports/rp2/pendsv.h \
+ /opt/local/lib/gcc/arm-none-eabi/12.1.0/include/stddef.h \
+ /Users/kevinthomas/Documents/pico-micropython-debug-template/pico/micropython/lib/pico-sdk/src/rp2_common/cmsis/stub/CMSIS/Device/RaspberryPi/RP2040/Include/RP2040.h \
+ /Users/kevinthomas/Documents/pico-micropython-debug-template/pico/micropython/lib/pico-sdk/src/rp2_common/cmsis/stub/CMSIS/Core/Include/core_cm0plus.h \
+ /opt/local/lib/gcc/arm-none-eabi/12.1.0/include/stdint.h \
+ /opt/local/arm-none-eabi/include/stdint.h \
+ /opt/local/arm-none-eabi/include/machine/_default_types.h \
+ /opt/local/arm-none-eabi/include/sys/_intsup.h \
+ /opt/local/arm-none-eabi/include/sys/_stdint.h \
+ /Users/kevinthomas/Documents/pico-micropython-debug-template/pico/micropython/lib/pico-sdk/src/rp2_common/cmsis/stub/CMSIS/Core/Include/cmsis_version.h \
+ /Users/kevinthomas/Documents/pico-micropython-debug-template/pico/micropython/lib/pico-sdk/src/rp2_common/cmsis/stub/CMSIS/Core/Include/cmsis_compiler.h \
+ /Users/kevinthomas/Documents/pico-micropython-debug-template/pico/micropython/lib/pico-sdk/src/rp2_common/cmsis/stub/CMSIS/Core/Include/cmsis_gcc.h \
+ /Users/kevinthomas/Documents/pico-micropython-debug-template/pico/micropython/lib/pico-sdk/src/rp2_common/cmsis/stub/CMSIS/Core/Include/mpu_armv7.h \
+ /Users/kevinthomas/Documents/pico-micropython-debug-template/pico/micropython/lib/pico-sdk/src/rp2_common/cmsis/stub/CMSIS/Device/RaspberryPi/RP2040/Include/system_RP2040.h
